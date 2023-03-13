546f4303afab232d0c2107aefc97ba91262c0500
Dekarter <dekarter@protonmail.com>
2023-03-13 12:20:53 +0300

    Добавлены решения для пунктов 10-13 задачи 2.1

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+import random, string
+
+def randomword(length):
+   letters = string.ascii_lowercase
+   return ''.join(random.choice(letters) for i in range(length))
+
 #Читаем файл в датафрейм df и выводим его
 df = pd.read_csv("dataset/sgemm_product.csv")
 print(df)
@@ -30,3 +36,22 @@ df.rename(columns={
     'Run4 (ms)': 'run4'
 }, inplace=True)
 print(df)
+
+df.to_csv('newcsv.csv', index=False)
+df2 = pd.read_csv('newcsv.csv')
+print(df2)
+
+print(df[df.run1 < 100.00])
+
+rndStr = ''
+for i in range(len(df.index)):
+    rndStr += randomword(4) + ' '
+df.insert(len(df.columns), 'strcol', rndStr.split())
+df2 = df[(df.run1 > 100.00) & (df.strcol.str.contains('a'))]
+print(df2)
+df2.to_csv('filteredds.csv')
+
+df_web = pd.read_html('https://developer.mozilla.org/en-US/docs/Learn/HTML/Tables/Basics')
+print(len(df_web))
+
+print(df_web[0])
